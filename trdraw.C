4f2db2b6fb72cafd6eb83df20583859a859d9270
E07 <survivalism.inoh@gmail.com>
2017-02-10 15:29:15 +0900

    apply slide mode

--- trdraw.C
+++ trdraw.C
@@ -1,3 +1,4 @@
+/* make inoh 2016 */
 #include <TCanvas.h>
 #include <TRint.h>
 #include <TGraph.h>
@@ -17,25 +18,58 @@ using namespace std;
 
 class MyApp : public TRint {
 public:
-	MyApp(int argc, char* argv[]) :
-	TRint("MyApp", &argc, argv,0,0,kTRUE),
-	mg1(),
-	c()
+	MyApp(int argc, char* filename[], int slide)
+	:	TRint("MyApp", &argc, filename,0,0,kTRUE)
+	,	mg1()
+	,	c()
 	{
 		if(Argc() == 1){
 			cout << "Call \"Graph Data File\". Error!" << endl;
 			exit(1);
 		}//if
 
-		Read();
+		Read(slide);
 	}
 
 	virtual ~MyApp(){
 	}
 
-	void Read(){
+	void Read(int slide){
 		TGraph *g[Argc()-1];
 		int k;
+
+	/* color settings */
+		const Int_t NCont = 50;
+		Int_t MyPalette[NCont];
+		int bin;
+		if(slide==0)bin=5;
+		else bin=4;
+		const Int_t NRGBs = bin;
+		Double_t red[NRGBs];
+		Double_t green[NRGBs];
+		Double_t blue[NRGBs];
+		Double_t stop[NRGBs];
+		if(slide==0){
+			red[0]=0.0;		red[1]=0.0;		red[2]=0.87;
+			red[3]=1.0;		red[4]=1.0;
+			green[0]=0.0;	green[1]=0.81;	green[2]=1.0;
+			green[3]=0.20;	green[4]=0.0;
+			blue[0]=0.51;	blue[1]=1.00;	blue[2]=0.12;
+			blue[3]=0.00;	blue[4]=0.00;
+			stop[0]=0.0;	stop[1]=0.25;	stop[2]=0.50;
+			stop[3]=0.75;	stop[4]=1.00;
+		}else{
+			red[0]=1.00;	red[1]=0.50;	red[2]=0.00;	red[3]=0.00;
+			green[0]=0.00;	green[1]=0.00;	green[2]=0.60;	green[3]=1.00;
+			blue[0]=0.00;	blue[1]=1.00;	blue[2]=1.00;	blue[3]=0.00;
+			stop[0]=0.00;	stop[1]=0.33;	stop[2]=0.64; stop[3]=1.00;
+		}//(if)else end
+//		cerr << red[0] << ":" << red[1] << ":" << red[2] << "..." << endl;
+		Int_t FI = TColor::CreateGradientColorTable(NRGBs,stop,red,green,blue,NCont);
+		for(int i=0;i<NCont;++i){
+			MyPalette[i] = FI+i;
+		}//for
+	/* color settings end */
 
 		for(k=1;k<Argc();k++){
 			cerr << "read " << Argv(k) << "..." << endl;
@@ -61,22 +95,13 @@ public:
 
 			data.close();
 
-			const Int_t NRGBs = 5;
-			const Int_t NCont = 255;
-			Int_t MyPalette[NCont];
-			Double_t red[NRGBs] = {0.0,0.0,0.87,1.0,1.0};
-			Double_t green[NRGBs] = {0.0,0.81,1.0,0.20,0.0};
-			Double_t blue[NRGBs] = {0.51,1.00,0.12,0.00,0.00};
-			Double_t stop[NRGBs] = {0.0,0.25,0.50,0.75,1.00};
-			Int_t FI = TColor::CreateGradientColorTable(NRGBs,stop,red,green,blue,NCont);
-			for(int i=0;i<NCont;++i){ MyPalette[i] = FI+i; }
-
 			g[k-1]->SetTitle(Argv(k));
 			g[k-1]->SetMarkerStyle(21);
 			g[k-1]->SetMarkerSize(0.6);
 			g[k-1]->SetLineWidth(1);
-			g[k-1]->SetMarkerColor(MyPalette[k*NCont/Argc()]);
-			g[k-1]->SetLineColor(1);//MyPalette[k*NCont/Argc()]);
+			g[k-1]->SetMarkerColor(MyPalette[(k-1)*NCont/(Argc()-2)]);
+			if(k==Argc()-1)g[k-1]->SetMarkerColor(MyPalette[NCont-2]);
+			g[k-1]->SetLineColor(1);//MyPalette[(k-1)*NCont/(Argc()-1)]);
 			
 			mg1.Add(g[k-1]);
 
@@ -87,9 +112,10 @@ public:
 
 	}
 		
-	void Runn(){
+	void Runn(int slide){
 		c.cd();
-		
+
+		if(slide!=0)SetWS(600,600);	
 		c.SetFillColor(0);
 //		c.SetFrameFillStyle(1);
 		c.SetFrameFillColor(kGray);
@@ -98,11 +124,11 @@ public:
 
 		mg1.Draw("APL");
 		mg1.SetTitle("RMS by Range");
+		if(slide!=0)mg1.SetTitle("Caluculation of #delta_{0}, Range#leq6mm, Step 100 #mum");
 		mg1.SetMinimum(0.);
 		mg1.GetYaxis()->SetTitle("#delta_{X'} [#mum]");
 		mg1.GetXaxis()->SetTitle("Range [#mum]");
 		gPad->Modified();
-		TLegend *tl = c.BuildLegend(0.55,0.75,0.9,0.9,"2nd diff");
 
 		mg1.GetXaxis()->SetTitleColor(kBlue+4);
 		mg1.GetYaxis()->SetLabelColor(kBlue+4);
@@ -111,13 +137,23 @@ public:
 		mg1.GetXaxis()->SetLabelColor(kBlue+4);
 		mg1.GetYaxis()->SetAxisColor(1);
 
-		tl->SetFillStyle(0);
-		tl->SetTextColor(kBlue+4);
-		tl->SetLineColor(1);
-		tl->SetLineWidth(2);
-	
+		if(slide==0){
+			TLegend *tl = c.BuildLegend(0.55,0.75,0.9,0.9,"2nd diff");
+			tl->SetFillStyle(0);
+			tl->SetTextColor(kBlue+4);
+			tl->SetLineColor(1);
+			tl->SetLineWidth(2);
+		}
 //		c.WaitPrimitive();
 	}
+
+void SetWP(Double_t wx,Double_t wy){
+	c.SetWindowPosition(wx,wy);
+}
+
+void SetWS(Double_t x,Double_t y){
+	c.SetWindowSize(x,y);
+}
 
 private:
 	TMultiGraph mg1;
@@ -125,8 +161,29 @@ private:
 };
 
 int main(int argc, char* argv[]){
-	MyApp app(argc,argv);
-	app.Runn();
+	if(argc<2){
+		cerr << "TrDraw [option] datfile(s)" << endl;
+		cerr << "datfile is writen by 2law (Range : dx)" << endl;
+		cerr << "Option: [-s] for showing on a slide" << endl;
+		exit(1);
+	}//if
+	
+	/* use for slide */
+	int slide = 0;
+	int param = 0;
+	if(strncmp(argv[1],"-s",2)==0){
+		slide++;
+		param++;
+	}
+	char *filename[argc-param];
+	filename[0]=argv[0];//imput program name
+	for(int index=1;index<argc-param;index++){
+		filename[index]=argv[index+param];
+	}//for
+	argc -= param;
+	
+	MyApp app(argc,filename,slide);
+	app.Runn(slide);
 	app.Run();
 	return 0;
 }
